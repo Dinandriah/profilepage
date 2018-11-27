10c4c1919d3c9c81db7569ac47334525b836b4b9
Dinandriah <dinandriah@gmail.com>
2018-11-27 11:13:38 -0700

    latest updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+// import WhatICanDo from './card'
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+
+// import game from './tictactoe.js'
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<WhatICanDo />, document.getElementById('root2'));
+registerServiceWorker();
